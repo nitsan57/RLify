c2abc17650eda9c2fcb81efa0d36548937ce9241
nitsan57 <nitsan57@gmail.com>
2023-08-21 08:12:01 +0300

    [Explorers] further improve of docs

--- rlify/agents/explorers.py
+++ rlify/agents/explorers.py
@@ -33,33 +33,50 @@ class Explorer(ABC):
 
 class RandomExplorer(Explorer):
     """Class that acts a linear exploration method"""
-    def __init__(self, exploration_epsilon=1, eps_end=0.05, eps_dec=1e-2) -> None:
+    def __init__(self, exploration_epsilon: int=1, eps_end: float=0.05, eps_dec: float=1e-2) -> None:
+        """
+        Args:
+            exploration_epsilon: The initial exploration epsilon
+            eps_end: The final exploration epsilon
+            eps_dec: The decay rate of the exploration epsilon
+        """
         self.exploration_epsilon = exploration_epsilon
         self.eps_end = eps_end
         self.eps_dec = eps_dec
 
 
     def explore(self):
+        """
+        Returns True if it is an exploration action time step (randomness based on the exploration epsilon)
+        """
         if np.random.random() < self.exploration_epsilon:
             return True
         return False
 
     def update(self):
+        """
+        updates the exploration epsilon in linear mode: exploration_epsilon * (1-self.eps_dec)
+        """
         self.exploration_epsilon = self.exploration_epsilon * (1-self.eps_dec) if self.exploration_epsilon > self.eps_end else self.eps_end
+
 
     def act(self, action_space, obs, num_obs):
         """
+        Args:
+            action_space: The action space of the env
+            obs: The observation of the env
+            num_obs: The number of observations to act on
         Reutns a random action from the action space
         """
         if np.issubdtype(action_space.dtype, np.integer):
-            return self.act_discrete(action_space, obs, num_obs)
+            return self._act_discrete(action_space, obs, num_obs)
         else:
-            return self.act_cont(action_space, obs, num_obs)
+            return self._act_cont(action_space, obs, num_obs)
 
-    def act_discrete(self, action_space, obs, num_obs):
+    def _act_discrete(self, action_space, obs, num_obs):
         return np.random.choice(action_space.n, num_obs)
     
-    def act_cont(self, action_space, obs, num_obs):
+    def _act_cont(self, action_space, obs, num_obs):
         res = []
         for i,low in enumerate(action_space.low):
             high = action_space.high[i]
@@ -92,8 +109,7 @@ class HeuristicExplorer(RandomExplorer):
 
     def act(self, action_space, obs, num_obs):
         """
-        Call the heuristic function to get the action
-
+        Call the heuristic function to get the action, also updates the inner state
         """
         res = self.heuristic_function(self.inner_state, obs)
         self.inner_state = res[0]
